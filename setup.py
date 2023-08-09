96a20758440313e3444a6938fa17abc4d0274f20
AddisonGambhir <136495646+AddisonGambhir@users.noreply.github.com>
2023-08-09 16:35:16 -0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='montecarlo',
+      version='0.1',
+      description='A package designed around several classes, die, game, and anlyzer, that creates games of customizable "dice" rolls, saves the game results to a dataframe, and analyzes the results of each game.',
+      url='https://github.com/AddisonGambhir/ajg7pk_ds5100_montecarlo.git',
+      author='Addison Gambhir',
+      author_email='ajg7pk@virginia.edu',
+      license='MIT',
+      packages=['ajg7pk_ds5100_montecarlo'])
